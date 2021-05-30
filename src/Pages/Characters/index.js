50dc246e1226a720285d7cb8466a3f7ad9df04e4
Alisson Vargas <alisson.vargass@hotmail.com>
2021-05-30 04:19:41 -0300

    feat: add search to Characters page

--- src/Pages/Characters/index.js
+++ src/Pages/Characters/index.js
@@ -1,5 +1,8 @@
-import { Container } from '@material-ui/core';
+import {
+  Container, IconButton, InputBase, Paper,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
 import { useEffect, useState } from 'react';
 import { Pagination, Skeleton } from '@material-ui/lab';
 import { useAuth } from '../../hooks/useAuth';
@@ -10,6 +13,15 @@ const useStyles = makeStyles((theme) => ({
   container: {
     display: 'flex',
     flexDirection: 'column',
+  },
+  formSearch: {
+    display: 'flex',
+    maxWidth: '250px',
+    margin: '10px',
+    backgroundColor: '#333',
+  },
+  searchInput: {
+    marginLeft: '5px',
   },
   characters: {
     display: 'flex',
@@ -32,11 +44,11 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Characters() {
   const classes = useStyles();
-
   const auth = useAuth();
 
   const [characters, setCharacters] = useState([]);
   const [favorites, setFavorites] = useState([]);
+  const [search, setSearch] = useState('');
   const [page, setPage] = useState(1);
   const [numPages, setNumPages] = useState(1);
   const [loading, setLoading] = useState(false);
@@ -45,7 +57,7 @@ export default function Characters() {
     setLoading(true);
 
     const offset = (page - 1) * 20;
-    const request = await fetch(`/api/characters?offset=${offset}`, {
+    const request = await fetch(`/api/characters?offset=${offset}&nameStartsWith=${search}`, {
       headers: {
         Authorization: `Bearer ${auth.user.token}`,
       },
@@ -69,6 +81,11 @@ export default function Characters() {
     setFavorites(favoriteCharacters);
   }, []);
 
+  function handleSearchSubmit(e) {
+    e.preventDefault();
+    getCharacters();
+  }
+
   async function handleFavoriteClick(id) {
     let favoritesCopy = [...favorites];
 
@@ -89,6 +106,19 @@ export default function Characters() {
   return (
     <Container className={classes.container}>
       <h1>Characters</h1>
+
+      <Paper component="form" className={classes.formSearch} onSubmit={handleSearchSubmit}>
+        <InputBase
+          className={classes.searchInput}
+          name="name"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <IconButton type="submit" aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
 
       <div className={classes.characters}>
         {loading
